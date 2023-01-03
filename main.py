c28d2cfc02dd5ba2d1e535c38abe44bcf7f1c34a
Hayashi-Yudai <yudai0913haya@gmail.com>
2023-01-03 07:31:17 +0000

    Append forward process

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-from dataclasses import dataclass
 from datetime import datetime
 import torch
 import torch.optim as optim
+from typing import List
 
 from data_loader import Dataset, Preprocessor, Sampler
 from model import NGCF
 from util import simple_logger
 
 
-@dataclass
 class Config:
     batch_size: int = 64
-    lr: float = 1e-5
     epoch: int = 10
+    emb_size: int = 64
+    layers: List[int] = [64, 64, 64]
 
-    node_dropout_flag: int = 1
+    lr: float = 1e-5
+    decay: float = 1e-5
+    node_dropout_flag: bool = True
+    node_dropout: List[float] = [0.1]
+    mess_dropout: List[float] = [0.1, 0.1, 0.1]
+
+    device: str = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 
 if __name__ == "__main__":
-    if torch.cuda.is_available():
-        device = "cuda:0"
-    else:
-        device = "cpu"
-    simple_logger(f"Use device {device}", __name__)
+    simple_logger(f"Use device {Config.device}", __name__)
 
     simple_logger("Initializing data loader", __name__)
     dataset = Dataset(path="./Data/gowalla")
@@ -31,7 +33,7 @@ if __name__ == "__main__":
     norm_adj = Preprocessor(dataset).get_adjacency_matrix()
 
     simple_logger("Initializing model", __name__)
-    model = NGCF(dataset.user_num, dataset.item_num, norm_adj).to(device)
+    model = NGCF(dataset.user_num, dataset.item_num, norm_adj, Config).to(Config.device)
     optimizer = optim.Adam(model.parameters(), lr=Config.lr)
     sampler = Sampler(dataset, batch_size=Config.batch_size)
 
@@ -42,9 +44,9 @@ if __name__ == "__main__":
 
         for idx in range(n_batch):
             users, pos_items, neg_items = sampler.sample()
-            # u_g_embeddings, pos_i_g_embeddings, neg_i_g_embeddings = model(
-            #     users, pos_items, neg_items, drop_flag=Config.node_dropout_flag
-            # )
+            u_g_embeddings, pos_i_g_embeddings, neg_i_g_embeddings = model(
+                users, pos_items, neg_items, drop_flag=Config.node_dropout_flag
+            )
             # batch_loss, batch_mf_loss, batch_emb_loss = model.create_bpr_loss(
             #     u_g_embeddings, pos_i_g_embeddings, neg_i_g_embeddings
             # )
--- model.py
+++ model.py
@@ -6,18 +6,25 @@ from typing import List
 
 
 class NGCF(nn.Module):
-    def __init__(self, n_user: int, n_item: int, norm_adj: sp.csr_matrix) -> None:
+    def __init__(
+        self, n_user: int, n_item: int, norm_adj: sp.csr_matrix, config
+    ) -> None:
         super(NGCF, self).__init__()
 
         self.n_user: int = n_user
         self.n_item: int = n_item
-        self.device: str = "cuda:0"  # TODO: Fix
-        self.emb_size: int = 64
-        self.batch_size: int = 64
-        self.layers: List[int] = [64, 64, 64]
-        self.decay: float = 1e-5
+        self.device: str = config.device
+        self.emb_size: int = config.emb_size
+        self.batch_size: int = config.batch_size
+        self.layers: List[int] = config.layers
+        self.decay: float = config.decay
+        self.mess_dropout: List[float] = config.mess_dropout
+        self.node_dropout: List[float] = config.node_dropout[0]
 
         self.norm_adj = norm_adj
+        self.sparse_norm_adj = self._convert_sp_mat_to_sp_tensor(self.norm_adj).to(
+            self.device
+        )
 
         self.embedding_dict, self.weight_dict = self.init_weight()
 
@@ -53,3 +60,71 @@ class NGCF(nn.Module):
             )
 
         return embedding_dict, weight_dict
+
+    def forward(
+        self,
+        users: List[int],
+        pos_items: List[int],
+        neg_items: List[int],
+        drop_flag: bool = True,
+    ):
+        if drop_flag:
+            A_hat = self.sparse_dropout(
+                self.sparse_norm_adj, self.node_dropout, self.sparse_norm_adj._nnz()
+            )
+        else:
+            A_hat = self.sparse_norm_adj
+
+        ego_embeddings = torch.cat(
+            [self.embedding_dict["user_emb"], self.embedding_dict["item_emb"]], 0
+        )
+        all_embeddings = [ego_embeddings]
+
+        for k in range(len(self.layers)):
+            side_embeddings = torch.sparse.mm(A_hat, ego_embeddings)
+            sum_embeddings = (
+                torch.matmul(side_embeddings, self.weight_dict[f"W_gc_{k}"])
+                + self.weight_dict[f"b_gc_{k}"]
+            )
+            bi_embeddings = torch.mul(ego_embeddings, side_embeddings)
+            bi_embeddings = (
+                torch.matmul(bi_embeddings, self.weight_dict[f"W_bi_{k}"])
+                + self.weight_dict[f"b_bi_{k}"]
+            )
+
+            ego_embeddings = nn.LeakyReLU(negative_slope=0.2)(
+                sum_embeddings + bi_embeddings
+            )
+            ego_embeddings = nn.Dropout(self.mess_dropout[k])(ego_embeddings)
+            norm_embeddings = F.normalize(ego_embeddings, p=2, dim=1)
+
+            all_embeddings += [norm_embeddings]
+
+        all_embeddings = torch.cat(all_embeddings, 1)
+        u_g_embeddings = all_embeddings[: self.n_user, :]
+        i_g_embeddings = all_embeddings[self.n_user :, :]
+
+        u_g_embeddings = u_g_embeddings[users, :]
+        pos_i_g_embeddings = i_g_embeddings[pos_items, :]
+        neg_i_g_embeddings = i_g_embeddings[neg_items, :]
+
+        return u_g_embeddings, pos_i_g_embeddings, neg_i_g_embeddings
+
+    def sparse_dropout(self, x, rate, noise_shape):
+        random_tensor = 1 - rate
+        random_tensor += torch.rand(noise_shape).to(x.device)
+        dropout_mask = torch.floor(random_tensor).type(torch.bool)
+        i = x._indices()
+        v = x._values()
+
+        i = i[:, dropout_mask]
+        v = v[dropout_mask]
+        out = torch.sparse.FloatTensor(i, v, x.shape).to(x.device)
+
+        return out * (1.0 / (1 - rate))
+
+    def _convert_sp_mat_to_sp_tensor(self, X):
+        coo = X.tocoo()
+        i = torch.LongTensor([coo.row, coo.col])
+        v = torch.from_numpy(coo.data).float()
+        return torch.sparse.FloatTensor(i, v, coo.shape)
