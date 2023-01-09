0b27a72e3137e92c2e0c74adb89f26fc75d11649
Hayashi-Yudai <yudai0913haya@gmail.com>
2023-01-09 13:00:03 +0000

    Fix

--- model.py
+++ model.py
@@ -143,16 +143,17 @@ class NGCF(nn.Module):
         pos_scores = torch.sum(torch.mul(users, pos_items), axis=1)
         neg_scores = torch.sum(torch.mul(users, neg_items), axis=1)
 
-        maxi = nn.LogSigmoid()(pos_scores - neg_scores)
-
-        mf_loss = -torch.mean(maxi)
+        mf_loss = -torch.sum(nn.LogSigmoid()(pos_scores - neg_scores))
 
         regularizer = (
             torch.norm(users) ** 2
             + torch.norm(pos_items) ** 2
             + torch.norm(neg_items) ** 2
-        ) / 2
-        emb_loss = self.decay * regularizer / self.batch_size
+        )
+        for w in self.weight_dict.values():
+            regularizer += torch.norm(w) ** 2
+
+        emb_loss = self.decay * regularizer
 
         return mf_loss + emb_loss, mf_loss, emb_loss
 
