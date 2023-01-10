635eec90d73136b6292418429c5fe6e8f54b82a8
Hayashi-Yudai <yudai0913haya@gmail.com>
2023-01-10 23:35:58 +0000

    Fix

--- model.py
+++ model.py
@@ -135,7 +135,7 @@ class NGCF(nn.Module):
 
     def _convert_sp_mat_to_sp_tensor(self, X):
         coo = X.tocoo()
-        i = torch.LongTensor(np.concatenate([coo.row, coo.col]))
+        i = torch.LongTensor([coo.row, coo.col])
         v = torch.from_numpy(coo.data).float()
         return torch.sparse.FloatTensor(i, v, coo.shape)
 
